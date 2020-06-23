828d195f0fc246e6625ea50fb68ef320bc88a1eb
Kevin Moore <moore.kevin.p@gmail.com>
2020-06-23 16:40:41 -0400

    requireAuth middleware function written

---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || '';
+  //Confirms there is an appropriate authToken and creates a bearerToken by slicing out the first portion
+  let bearerToken;
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' });
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
+  }
+  //Takes the verified bearerToken and compares it to the user_id, providing a 401 if there is no match
+  try {
+    const payload = AuthService.verifyJwt(bearerToken);
+    AuthService.getUserWithId(
+      req.app.get('db'),
+      payload.user_id
+    )
+      .then(user => {
+        if (!user)
+          return res.status(401).json({ error: 'Unauthorized request' });
+
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        console.error(err);
+        next(err);
+      });
+  } catch(error) {
+    res.status(401).json({ error: 'Unauthorized request' });
+  }
+}
+  
+module.exports = {
+  requireAuth,
+};
